ec47c6d36d69cd19e146ebc6cd0666832f4ceef4
Bryan <bryanbscott2000@gmail.com>
2015-05-16 11:36:30 -0400

    made changes to organize the home screen, also hooked up the example Movie API  form to the endpoint. Added basic form validtion to the example form.

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@
  * Created by bscott3 on 5/16/2015.
  */
 var app_controllers = angular.module('app.controllers', []);
-app_controllers.controller('AppCtrl', function($scope){
+app_controllers.controller('AppCtrl', function($scope,$http){
     $scope.MovieApiMethods = [
         {
             api_path:"listMovies",
@@ -26,19 +26,38 @@ app_controllers.controller('AppCtrl', function($scope){
     updateClock();
 
     $scope.favoriteMovies = [];
-    $scope.addMovie = function(){
+    $http.get('https://random-development.appspot.com/_ah/api/movie/v1/listMovies').then(function(resp){
+        $scope.favoriteMovies = resp.data.items;
+    },function(err){
+        console.log(err)
+    })
+
+    $scope.addMovie = function(isValid){
         var title = $scope.m_title;
         var main_char = $scope.main_char;
         var genre = $scope.genre;
         //We can use this data object to post the data to a server
       var data;
         data={};
-        data["title"] = title;
+        data["movie_title"] = title;
         data["main_char"] = main_char;
         data["genre"] = genre;
         console.log(data)
-        $scope.favoriteMovies.push(data)
+        if(isValid) {
+            $http.post('https://random-development.appspot.com/_ah/api/movie/v1/postMovie', data).then(function (resp) {
+                console.log(resp)
+                $scope.m_title = '';
+                $scope.main_char = '';
+                $scope.genre = '';
+            }, function (err) {
+                console.log(err)
+            });
+            $scope.favoriteMovies.push(data)
+        }else{
+            console.log("Form invalid")
+        }
     }
+
 
 });
 
